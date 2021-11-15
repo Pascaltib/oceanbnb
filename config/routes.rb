d8e0806ccfb5d5d4f15c08ce109445c90d69448e
Pascal Tiberghien <pascaltib@gmail.com>
2021-11-15 14:46:07 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'profile', to: 'pages#profile'
+
+  resources :yachts, only: [:index, :show]
+
+  resources :bookings, only: [:create] do
+    member do
+      patch :accept
+      patch :reject
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_120051) do
+ActiveRecord::Schema.define(version: 2021_11_15_134010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_11_15_120051) do
     t.bigint "yacht_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status", default: "pending"
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["yacht_id"], name: "index_bookings_on_yacht_id"
   end

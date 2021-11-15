e2f8aaceaf3e316d23f4098923e4ce1d74471d23
rayvenrey <rayven.rey@gmail.com>
2021-11-15 15:55:56 +0100

    show view structure

--- app/controllers/yachts_controller.rb
+++ app/controllers/yachts_controller.rb
@@ -1,3 +1,13 @@
 class YachtsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show, :index]
+
+  def show
+    @yacht = Yacht.find(params[:id])
+    @reviews = Review.new
+  end
+
+  private
+  def yacht_params
+    params.require(:yacht).permit(:name, :description, :capacity, :location, :image_url)
+  end
 end

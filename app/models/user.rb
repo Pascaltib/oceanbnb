f1b1153c456fdce3c55f88193b292148d9bdb482
Pascal Tiberghien <pascaltib@gmail.com>
2021-11-15 15:28:41 +0100

    relationships between models defined

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :bookings
+  has_many :yachts, through: :bookings, dependent: :destroy
+
+  validates :fist_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :location, presence: true
 end

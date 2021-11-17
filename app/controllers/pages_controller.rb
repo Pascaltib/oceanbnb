52b44af9e0ad8a8cc29660aec0e793ce9dc72c89
rayvenrey <rayven.rey@gmail.com>
2021-11-17 12:29:05 +0100

    sadasdasd

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ class PagesController < ApplicationController
     @users = User.all
     @user = current_user
 # added to access the yatch to profile
-    @yachts = Yacht.all
+    @user_bookings = Booking.all.select { |booking| booking.user_id == @user.id}
     # @user.yachts = date_reserved??
   end
 end

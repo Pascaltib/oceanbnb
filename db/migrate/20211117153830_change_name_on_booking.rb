38cab80adb8bc6eb7de27bb198502d351a0faa90
Pascal Tiberghien <pascaltib@gmail.com>
2021-11-17 16:48:20 +0100

    change

--- db/migrate/20211117153830_change_name_on_booking.rb
+++
@@ -1,6 +0,0 @@
-class ChangeNameOnBooking < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :bookings, :starts_at, :start_date
-    rename_column :bookings, :ends_at, :end_date
-  end
-end

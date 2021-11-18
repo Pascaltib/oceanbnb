6d4ccde9f805baf7dff5ee4fe1709c20a56cb6bc
Pascal Tiberghien <pascaltib@gmail.com>
2021-11-18 12:00:45 +0100

    price logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,15 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @yacht1 = Yacht.all.sample
+    @yacht2 = Yacht.all.sample
   end
 
   def profile
     @users = User.all
     @yachts = Yacht.all
     @user = current_user
-# added to access the yatch to profile
+    # added to access the yatch to profile
     @user_bookings = Booking.all.select { |booking| booking.user_id == @user.id}
     # @user.yachts = date_reserved??
   end
